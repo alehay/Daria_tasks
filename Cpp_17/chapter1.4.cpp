f5873623ec03e465fa8eb1dce5dad7e0fb0c4253
Alehay <alehaymkm@gmail.com>
2021-01-02 16:15:16 +0300

    sloved 1.4 and 1.5

--- Cpp_17/chapter1.4.cpp
+++ Cpp_17/chapter1.4.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 
+
+
+
+template<typename N, typename I, typename S>
+struct info{
+  N name;
+  I id;
+  S salary;
+
+  explicit info(N n, I i, S s) :
+    name{n}, id{i}, salary{s}
+  {}
+};
+
+// мой код : 
+template<typename N, typename I, typename S> 
+info <N, I, S> make_info (N n, I i, S s) {
+  return info<N, I, S>(n, i, s);  // без Дарьи я бы и не решил. 
+}
+
 int main(){
+  // Daria - Разрешается компилировать только с флагом -std=c++14
+  // Найти и устранить проблему компиляции.
+
+  auto worker(make_info("John", 32, 1000));
+  std::cout << "#" << worker.id << "\tName: " << worker.name
+            << "\tSalary: " << worker.salary << std::endl;
+  
 }
