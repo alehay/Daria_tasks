2ba960b47ceccc53d2e8036056b70a6ab548ec6e
Alehay <alehaymkm@gmail.com>
2021-02-02 21:23:00 +0300

    file

--- cpp_other/task_2.cpp
+++ cpp_other/task_2.cpp
@@ -6,6 +6,17 @@
 #include <iostream>
 #include <vector>
 
+#include <boost/iterator/zip_iterator.hpp>
+#include <boost/fusion/algorithm/transformation/zip.hpp>
+#include <boost/fusion/include/zip.hpp>
+#include <boost/range.hpp>
+
+#include <boost/range/combine.hpp>
+#include <boost/foreach.hpp>
+
+
+#include <iterator>
+
 int main() {
   std::vector<double> v1 = {1,2,3};
   std::vector<double> v2 = {2,4,6};
@@ -21,9 +32,23 @@ int main() {
 ////////////////////////////////////////////////
 // разделить все элементы вектора v1 на элементы
 // вектора  v2 почленно.   
-  for_each(...);
+//  for_each(...);
+//auto zip = std::zip( v1 , v2 );
+  //for( auto & [ v_1,  v_2] : zip( v1 , v2 )) {
+  //  std::cout << v_1  << v_2 << std::endl;
+  //for (auto && items : boost::fusion::zip ( v1 , v2 ))  {
+    //for (auto [v_1, v_2] : zip( v1 , v2 ) ) {
+    //for (auto zi : zip ( v1 , v2 ) ) {
+    for (auto const & i : boost::combine(v1 , v2) ) {
+      double v_1;
+      double v_2;
+      boost::tie(v_1,v_2) = i;
+      std::cout << v_1  << v_2 << std::endl;
+    }
+  }
+
 ////////////////////////////////////////////////  
-  
+  /*
   for(auto&& v : v1) {
     std::cout << v << ",";
   }
@@ -31,8 +56,8 @@ int main() {
   for(auto&& v : v2) {
     std::cout << v << ",";
   }
-  std::cout << "\n";
-}
+  std::cout << "\n"; */
+} 
 
 /*
 И распечатать внутри for_each. Вывод:
